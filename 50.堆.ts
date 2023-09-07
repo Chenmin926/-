fa72d275609820548b2837cff2f96654d2931edb
chenmin <940827423@qq.com>
2023-09-08 00:21:43 +0800

    大根堆的heapify，以及堆排序原理

--- 50.堆.ts
+++ 50.堆.ts
@@ -1,11 +1,12 @@
 // 使用数组来构建一个大根堆
 class bigHeap{
   heap: number[] = [];
+  heapSize: number = 0;
 
   add(num: number) {
     this.heap.push(num)
-    let length = this.heap.length;
-    length && this.heapInsert(length - 1)
+    this.heapSize++;
+    this.heapSize && this.heapInsert(this.heapSize - 1)
     console.log(this.heap)
   }
 
@@ -16,6 +17,38 @@ class bigHeap{
       this.swap(index, parentIndex);
       index = parentIndex
       parentIndex = Math.floor((parentIndex - 1) / 2)
+    }
+  }
+
+  poll() {
+    if(!this.heapSize) return
+    let res = this.heap[0]
+    this.heap[0] = this.heap[this.heapSize - 1];
+    this.heap[this.heapSize - 1] = res;
+    this.heap.pop()
+    this.heapSize--;
+    this.heapify(0);
+    console.log(res,this.heap)
+    return res
+  }
+
+  heapify(index: number) {
+    // 左孩子索引
+    let left = (2 * index) + 1;
+    // 左孩子不越界
+    while (left < this.heapSize) {
+      // 判断是否有右孩子 有的话比较两者
+      let largest = left + 1 < this.heapSize ? this.heap[left] < this.heap[left + 1] ? left + 1 : left : left;
+      // 判断较大孩子是否比父亲大
+      largest = this.heap[largest] > this.heap[index] ? largest : index;
+      // 自身已经比较大孩子大 不需要继续往下找
+      if (largest === index) {
+        break;
+      }
+      // 有比自己大的孩子
+      this.swap(index, largest);
+      index = largest;
+      left = (index * 2) + 1;
     }
   }
 
@@ -31,3 +64,5 @@ b1.add(10)
 b1.add(8)
 b1.add(9)
 b1.add(11)
+b1.poll()
+b1.poll()
